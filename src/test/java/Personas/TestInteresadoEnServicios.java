f75d190fc063d3e0170c656161e5fbad9746ba33
Avril Dinora Martina Lopez <amartinalopez@frba.utn.edu.ar>
2023-06-03 21:32:22 -0300

    agrego testearAgregarLocalizacion para no repetir codigo

--- src/test/java/Personas/TestInteresadoEnServicios.java
+++ src/test/java/Personas/TestInteresadoEnServicios.java
@@ -1,6 +1,7 @@
 package Personas;
 
 import domain.Personas.InteresadoEnServicios;
+import domain.Seguridad.RegistroDeUsuarioException;
 import domain.Usuario.RepositorioDeUsuarios;
 import domain.Usuario.Usuario;
 import org.junit.jupiter.api.Assertions;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import services.Localizacion.Departamento;
+import services.Localizacion.Localizacion;
 import services.Localizacion.Municipio;
 import services.Localizacion.Provincia;
 
@@ -16,6 +18,15 @@ import java.util.List;
 public class TestInteresadoEnServicios {
 
     static InteresadoEnServicios interesado1;
+
+    public void testearAgregarLocalizacion(InteresadoEnServicios persona, Localizacion localizacion) {
+
+        Assertions.assertEquals(localizacion.id,persona.getLocalizaciones().get(0).id);
+
+        Assertions.assertEquals(localizacion.nombre,persona.getLocalizaciones().get(0).nombre);
+        //Assertions.assertSame(localizacion,persona.getLocalizaciones().get(0));
+        //Assertions.assertTrue(localizacion.equals(persona.getLocalizaciones().get(0)));
+    }
     @BeforeAll
     public static void init1(){
         interesado1 = new InteresadoEnServicios("perez","jose","perezjose@gmail.com");
@@ -24,36 +35,21 @@ public class TestInteresadoEnServicios {
     @Test
     public void testAgregarProvincia(){
         Provincia santiagoDelEstero = new Provincia(86);
-
         interesado1.agregarLocalizacion(86,0,0);
-
-        Assertions.assertEquals(santiagoDelEstero.id,interesado1.getLocalizaciones().get(0).id);
-
-        Assertions.assertEquals(santiagoDelEstero.nombre,interesado1.getLocalizaciones().get(0).nombre);
-        //Assertions.assertSame(santiagoDelEstero,interesado1.getLocalizaciones().get(0));
+        testearAgregarLocalizacion(interesado1,santiagoDelEstero);
         }
     @Test
     public void testAgregarDepartamento(){
         Departamento belen = new Departamento(10, 10035);
-
         interesado1.agregarLocalizacion(10,10035,0);
-
-        Assertions.assertEquals(belen.id,interesado1.getLocalizaciones().get(0).id);
-
-        Assertions.assertEquals(belen.nombre,interesado1.getLocalizaciones().get(0).nombre);
-        //Assertions.assertSame(santiagoDelEstero,interesado1.getLocalizaciones().get(0));
+        testearAgregarLocalizacion(interesado1,belen);
     }
 
     @Test
     public void testAgregarMunicipio(){
         Municipio hualfin = new Municipio(10, 10035,100077);
-
         interesado1.agregarLocalizacion(10,10035,100077);
-
-        Assertions.assertEquals(hualfin.id,interesado1.getLocalizaciones().get(0).id);
-
-        Assertions.assertEquals(hualfin.nombre,interesado1.getLocalizaciones().get(0).nombre);
-        //Assertions.assertSame(santiagoDelEstero,interesado1.getLocalizaciones().get(0));
+        testearAgregarLocalizacion(interesado1,hualfin);
     }
 
 }
