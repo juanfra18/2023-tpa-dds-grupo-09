402454e45de1eab0f731629e3cf72177803149bc
diegohcanetti <dhernandezcanetti@frba.utn.edu.ar>
2023-09-10 12:20:09 -0300

    Repo Posicion

--- src/main/java/domain/Incidentes/Posicion.java
+++ src/main/java/domain/Incidentes/Posicion.java
@@ -14,12 +14,14 @@ public class Posicion extends Persistente {
     private double latitud;
     @Column(name = "longitud")
     private double longitud;
-    public Posicion(String coordenadas) { //Ingresa en formato "40.7486,-73.9864"
+    public Posicion() {} //Revisar TODO
+    public double distancia(Posicion posicion) {
+        return Math.sqrt(Math.pow(this.latitud-posicion.getLatitud(), 2)+Math.pow(this.longitud-posicion.getLongitud(), 2));
+    }
+
+    public void setPosicion(String coordenadas) { //Ingresa en formato "40.7486,-73.9864"
         String[] aux = coordenadas.split(",");
         this.latitud = Double.valueOf(aux[0]);
         this.longitud = Double.valueOf(aux[1]);
-    }
-    public double distancia(Posicion posicion) {
-        return Math.sqrt(Math.pow(this.latitud-posicion.getLatitud(), 2)+Math.pow(this.longitud-posicion.getLongitud(), 2));
     }
 }
---
+++ src/main/java/domain/Persistencia/Repositorios/RepositorioPosicion.java
@@ -0,0 +1,55 @@
+package domain.Persistencia.Repositorios;
+
+import domain.Incidentes.Posicion;
+import domain.Notificaciones.*;
+import domain.Personas.Comunidad;
+import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
+
+import javax.persistence.EntityTransaction;
+import java.util.List;
+
+public class RepositorioPosicion implements WithSimplePersistenceUnit {
+    private EntityTransaction tx;
+    public RepositorioPosicion(){
+        this.tx = entityManager().getTransaction();
+    }
+
+    public static void main(String[] args) {
+        RepositorioPosicion repo = new RepositorioPosicion();
+        Posicion posicion = new Posicion();
+        posicion.setPosicion("40.7486,-73.9864");
+
+        repo.agregar(posicion);
+
+        posicion.setPosicion("40.7973,-73.9864");
+
+        repo.modificar(posicion);
+
+        System.out.println(repo.buscar(posicion.getId()).distancia(posicion));
+
+        System.out.println(repo.buscarTodos().size());
+
+        //repo.eliminar(comunidad);
+    }
+    public void agregar(Posicion posicion) {
+        this.tx.begin();
+        entityManager().persist(posicion);
+        this.tx.commit();
+    }
+    public void modificar(Posicion posicion) {
+        this.tx.begin();
+        entityManager().merge(posicion);
+        this.tx.commit();
+    }
+    public void eliminar(Posicion posicion) {
+        this.tx.begin();
+        entityManager().remove(posicion);
+        this.tx.commit();
+    }
+    public Posicion buscar(long id){
+        return entityManager().find(Posicion.class, id);
+    }
+    public List<Posicion> buscarTodos(){
+        return entityManager().createQuery("from Posicion", Posicion.class).getResultList();
+    }
+}
