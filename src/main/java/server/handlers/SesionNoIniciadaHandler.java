b7def7a9086597468cb7bb7afabc645c2ed7a1ff
rami-nava <raminava60@gmail.com>
2023-10-30 18:03:29 -0300

    Update SesionNoIniciadaHandler.java

--- src/main/java/server/handlers/SesionNoIniciadaHandler.java
+++ src/main/java/server/handlers/SesionNoIniciadaHandler.java
@@ -8,7 +8,11 @@ public class SesionNoIniciadaHandler implements IHandler {
   @Override
   public void setHandle(Javalin app) {
     app.exception(SesionNoIniciadaExcepcion.class, (e, context) -> {
-      context.redirect("/");
+      if(context.path().equals("/inicioDeSesion")) {
+        context.redirect("/inicioDeSesion");
+      }else {
+        context.redirect("/");
+      }
     });
   }
 }
