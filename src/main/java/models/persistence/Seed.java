a92dcb8517bdc55f8ca76a903291e2883291b1ea
juanfra18 <juanpaoli@gmail.com>
2023-12-05 17:27:09 -0300

    Borrado seed 2 y seed con solo un admin

--- src/main/java/models/persistence/Seed.java
+++ src/main/java/models/persistence/Seed.java
@@ -72,25 +72,13 @@ public class Seed implements WithSimplePersistenceUnit {
                     provincia -> servicioGeoref.listadoDeMunicipiosDeProvincia(provincia).
                             getMunicipios().forEach(municipio -> repositorioDeMunicipios.agregar(municipio)));
 
-            Posicion posicion = new Posicion();
-            posicion.setPosicion("-34.6078602,-58.383111");
-
-            RepositorioPosicion.getInstancia().agregar(posicion);
-
-
             //PARA PROBAR LA PAGINA WEB
             RepositorioDeUsuarios repositorioDeUsuarios = RepositorioDeUsuarios.getInstancia();
             RepositorioRoles repositorioRoles = RepositorioRoles.getInstancia();
             Usuario u1 = new Usuario();
-            Usuario u2 = new Usuario();
-            Usuario u3 = new Usuario();
             u1.setUsername("messi10");
-            u2.setUsername("kuni9");
-            u3.setUsername("dibu23");
             u1.setContrasenia("Grupodisenio@");
-            u2.setContrasenia("Grupodisenio@");
-            u3.setContrasenia("Grupodisenio@");
-
+            
             Rol admin = new Rol();
             admin.setNombre("administrador");
             admin.setTipo(TipoRol.ADMINISTRADOR);
@@ -106,12 +94,8 @@ public class Seed implements WithSimplePersistenceUnit {
             repositorioRoles.agregar(empresa);
 
             u1.setRol(admin);
-            u2.setRol(basico);
-            u3.setRol(basico);
 
             repositorioDeUsuarios.agregar(u1);
-            repositorioDeUsuarios.agregar(u2);
-            repositorioDeUsuarios.agregar(u3);
 
             RepositorioMiembroDeComunidad repositorioMiembroDeComunidad = RepositorioMiembroDeComunidad.getInstancia();
             MiembroDeComunidad mi = new MiembroDeComunidad();
@@ -119,54 +103,16 @@ public class Seed implements WithSimplePersistenceUnit {
             mi.setNombre("Leo");
             mi.setUsuario(u1);
 
-            MiembroDeComunidad mi2 = new MiembroDeComunidad();
-            mi2.setApellido("Kun");
-            mi2.setNombre("Aguero");
-
-            MiembroDeComunidad mi3 = new MiembroDeComunidad();
-            mi3.setApellido("Emiliano");
-            mi3.setNombre("Martinez");
-
             ReceptorDeNotificaciones receptorDeNotificaciones1 = new ReceptorDeNotificaciones();
             receptorDeNotificaciones1.setMail("diseniogrupo9@gmail.com");
             receptorDeNotificaciones1.setTelefono("01144444444");
             receptorDeNotificaciones1.cambiarFormaDeNotificar(new CuandoSuceden());
             receptorDeNotificaciones1.cambiarMedioDeComunicacion(new ViaMail());
 
-            ReceptorDeNotificaciones receptorDeNotificaciones2 = new ReceptorDeNotificaciones();
-            receptorDeNotificaciones2.setMail("diseniogrupo9@gmail.com");
-            receptorDeNotificaciones2.setTelefono("01121212121");
-            receptorDeNotificaciones2.cambiarFormaDeNotificar(new CuandoSuceden());
-            receptorDeNotificaciones2.cambiarMedioDeComunicacion(new ViaMail());
-
-            ReceptorDeNotificaciones receptorDeNotificaciones3 = new ReceptorDeNotificaciones();
-            receptorDeNotificaciones3.setMail("diseniogrupo9@gmail.com");
-            receptorDeNotificaciones3.setTelefono("01132432424");
-            receptorDeNotificaciones3.cambiarFormaDeNotificar(new CuandoSuceden());
-            receptorDeNotificaciones3.cambiarMedioDeComunicacion(new ViaMail());
-
             mi.setReceptorDeNotificaciones(receptorDeNotificaciones1);
             mi.setUsuario(u1);
 
-            mi2.setReceptorDeNotificaciones(receptorDeNotificaciones2);
-            mi2.setUsuario(u2);
-
-            mi3.setReceptorDeNotificaciones(receptorDeNotificaciones3);
-            mi3.setUsuario(u3);
-
             repositorioMiembroDeComunidad.agregar(mi);
-            repositorioMiembroDeComunidad.agregar(mi2);
-            repositorioMiembroDeComunidad.agregar(mi3);
-
-            Comunidad c1 = new Comunidad();
-            c1.setNombre("messistas");
-
-            Comunidad c2 = new Comunidad();
-            c2.setNombre("seleccionArg");
-
-            RepositorioComunidad repositorioComunidad = RepositorioComunidad.getInstancia();
-            repositorioComunidad.agregar(c1);
-            repositorioComunidad.agregar(c2);
 
             em.getTransaction().commit();
             em.close();
