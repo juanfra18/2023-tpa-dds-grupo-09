be7a07246ffd8260dccf9c9c83ff0dddc3694503
rami-nava <raminava60@gmail.com>
2023-12-18 17:31:21 -0300

    Test usuario funcional

--- src/test/java/Usuario/TestUsuario.java
+++ src/test/java/Usuario/TestUsuario.java
@@ -1,23 +1,32 @@
 package Usuario;
 
 import models.domain.Seguridad.RegistroDeUsuarioException;
+import models.persistence.EntityManagerSingleton;
 import models.persistence.Repositorios.RepositorioDeUsuarios;
 import models.domain.Usuario.Usuario;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-public class TestUsuario {/*
+import javax.persistence.EntityManager;
+
+public class TestUsuario {
     static RepositorioDeUsuarios repo;
     static Usuario usuario1;
+    static EntityManager em;
 
     @BeforeAll
     public static void init1(){
+        em = EntityManagerSingleton.getInstance();
+        em.getTransaction().begin();
         usuario1 = new Usuario();
         usuario1.setUsername("pedrito");
         usuario1.cambiarContrasenia("HolaMundo@3");
         repo = RepositorioDeUsuarios.getInstancia();
-        //repo.agregarUsername(usuario1);
+        repo.agregarUsername(usuario1);
+    }
+
+    @AfterAll
+    public static void fin1(){
+        em.getTransaction().rollback();
     }
 
     @Test
@@ -27,6 +36,8 @@ public class TestUsuario {/*
             usuario1.cambiarContrasenia(nuevaContrasenia);
         });
     }
+
+
     @Test
     public void testRepositorioExito(){ //es exitoso porque logra almacenar 2 usernames ya que estos son distintos
         Usuario usuario2 = new Usuario();
@@ -36,11 +47,12 @@ public class TestUsuario {/*
             repo.agregarUsername(usuario2);
         });
     }
+
     @Test
     public void testRepositorioFalla() throws RegistroDeUsuarioException { //falla ya que ambos usernames son iguales
         Usuario usuario2 = new Usuario();
         usuario2.setUsername("pedrito" );
         usuario2.cambiarContrasenia("HolaMundo@3");
         Assertions.assertThrows(RegistroDeUsuarioException.class, () -> repo.agregarUsername(usuario2));
-    }*/
+    }
 }
