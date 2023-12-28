6d08ee7567bd4592adbfa8ba76e3ed63249dbaad
juanfra18 <juanpaoli@gmail.com>
2023-12-28 16:33:16 -0300

    Eliminado archivo ranking 3

--- src/main/java/models/Config/Config.java
+++ src/main/java/models/Config/Config.java
@@ -15,6 +15,5 @@ public class Config {
   public static final String URL_APIS2 = "http://localhost:8080/";
   public static final String ARCHIVO_CSV_RECIBIDO = "resources/";
   public static final Long TIMEOUT = 200L;
-  public static final String RANKING_3 = "resources/ranking3.csv";
   public static final Long TIMELOCATION = 600L;
 }
